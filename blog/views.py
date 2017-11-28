a37e5d380875156211c9526295fde911fe3abf72
Noordle <noordle1537@gmail.com>
2017-11-28 17:29:28 +0300

    Edit
    Signed-off-by: Noordle <noordle1537@gmail.com>

--- blog/views.py
+++ blog/views.py
@@ -6,13 +6,15 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 import datetime
 from django.contrib.auth import login, logout
 from django.views.generic.base import View
+import json
 
 
 def post_all(request):
     object_list = Post.objects.order_by('date')
+    count_post = Post.objects.filter(author=request.user).count()
     template = loader.get_template('blog/post_list.html')
     context = {
-        'object_list': object_list, 'user': request.user
+        'object_list': object_list, 'user': request.user, 'count_post' : count_post,
     }
     return HttpResponse(template.render(context))
 
@@ -28,11 +30,11 @@ def post_view(request, post_id):
         context = {'object_list_com': object_list_com, 'post': post, 'user': request.user}
         return HttpResponse(template.render(context))
     elif request.method == 'PUT':
-        # message_value = json.loads(request.body)
-        # message = request.PUT.get(request.body)
         post = Post.objects.get(id=post_id)
-        post.message = request.body
-        return HttpResponseRedirect(post.get_absolute_url()+'/')
+        changedmessage = json.loads(request.body.decode())
+        post.message = changedmessage['message_izm']
+        post.save()
+        return HttpResponseRedirect(post.get_absolute_url())
 
 def add(request):
     if request.method == "POST":
